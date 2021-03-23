69c4d8c78396ea6bb7e475e6b82ed7fbd543b0b8
RomanNunez <roman_nunez@ymail.com>
2021-03-23 11:41:07 -0500

    generate page users

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user.model';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // 4. Set the domain portion of the url
+  private url:string = 'http://localhost:3000/api/auth';
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  // 5. Replace the test method with a working implementation of login.
+  login(user: User): Observable<User> {
+    return this.http.post<User>(`${this.url}/login`, user, httpOptions);
+  }
+  logout(user: User): Observable<User> {
+    return this.http.post<User>(`${this.url}/logout`, user, httpOptions);
+}
 }
